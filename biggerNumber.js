c545094d647cd562090dee24e46119b85ab29ffd
Evgeny <evgen.babenko@gmail.com>
2018-05-01 01:33:24 +0300

    fix minor bug

---
+++ biggerNumber.js
@@ -0,0 +1,27 @@
+/*
+Завдання 5 - Наступне більше число (складність 5/5)
+
+Завдання: вам потрібно написати функцію яка приймає додатнє число і повертає наступне більше число, утворене з тих самих цифер.
+
+Тести:
+
+biggerNumber(23) // 32;
+biggerNumber(624) // 642;
+biggerNumber(2018) // 2081;
+
+Якщо більшого числа немає, повернути -1
+
+biggerNumber(9) = -1;
+biggerNumber(111) = -1;
+biggerNumber(531) = -1;
+*/
+
+function biggerNumber(number) {
+    const res = -1;
+
+    
+    const arr = number.split('');
+    return res;
+}
+
+biggerNumber(2018)
